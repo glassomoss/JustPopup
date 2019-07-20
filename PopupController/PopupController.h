6d0f728d32bcdbdd53dbdb2f71cca52c14ebfbe9
Mefodiy <akatpod2@mail.ru>
2019-07-21 02:30:39 +0300

    Initial Commit

---
+++ PopupController/PopupController.h
@@ -0,0 +1,19 @@
+//
+//  PopupController.h
+//  PopupController
+//
+//  Created by Валерий Акатов on 21.07.2019.
+//  Copyright © 2019 Eubicor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopupController.
+FOUNDATION_EXPORT double PopupControllerVersionNumber;
+
+//! Project version string for PopupController.
+FOUNDATION_EXPORT const unsigned char PopupControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupController/PublicHeader.h>
+
+
