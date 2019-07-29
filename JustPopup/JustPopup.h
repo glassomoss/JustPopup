7c2c6f0594ce10719007191021f4ebb550fb9c06
glassomoss <akatpod2@mail.ru>
2019-07-29 03:23:19 +0300

    Just updated everything

--- JustPopup/JustPopup.h
+++ JustPopup/JustPopup.h
@@ -9,11 +9,11 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for PopupController.
-FOUNDATION_EXPORT double PopupControllerVersionNumber;
+FOUNDATION_EXPORT double JustPopupVersionNumber;
 
 //! Project version string for PopupController.
-FOUNDATION_EXPORT const unsigned char PopupControllerVersionString[];
+FOUNDATION_EXPORT const unsigned char JustPopupVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PopupController/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <JustPopup/PublicHeader.h>
 
 
